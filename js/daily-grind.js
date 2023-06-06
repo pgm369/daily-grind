286facddcbd5a6b3b18c0bfb9e6f6cc2dba44d89
Replit user <>
2023-06-06 19:45:37 +0000

    updates changes

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -13,49 +13,51 @@ day - the day of the week
 */
 
 function coffeeTemplate(coffee) {
-  return `<p>
-    		 <img src="${coffee.pic}" alt="${coffee.pic}"    id="coffee" />
-            <strong class="feature">${coffee.pic}'s Coffee Special:</strong> ${coffee.pic}'s daily coffee special is <strong class="feature">Pumpkin Spice Latte</strong>, which makes us wish it was always Fall, as this is one of our top sellers!</p>
+
+  return `
+  <p>
+    		 <img src="${coffee.pic}" alt="${coffee.alt}"    id="coffee" />
+            <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
             `;
-}
-
-let myDate = new Date();
-
-let today = myDate.getDay();
-//today = 3;	
-
-switch (today) {
-
- case 1:
-    today = "Monday";
-    coffee = {
-      name: "Bubble Tea",
-      color: "pink",
-      pic: "images/bubble-tea.jpg",
-      alt: "A picture of a Bubble Tea",
-      day: "Monday",
-      desc: `I like me some bubble tea!` //no comma needed if last
-    };
-    break;
-
-  case 2:
-    today = "Tuesday";
-    break;
-
-  case 3:
-    today = "Wednesday";
-    break;
-
-  default:
-    today = "Not sure what day it is!";
-
-  
-
-}
-
-document.getElementByID("coffee-cup").innerHTML = coffeeTemplate(coffee);
-coffee
 
 
-//alert(today);
-console.log(coffee); 
+  let myDate = new Date();
+
+  let today = myDate.getDay();
+  //today = 3;	
+
+  switch (today) {
+
+    case 1:
+      today = "Monday";
+      coffee = {
+        name: "Bubble Tea",
+        color: "pink",
+        pic: "images/bubble-tea.jpg",
+        alt: "A picture of a Bubble Tea",
+        day: "Monday",
+        desc: `I like me some bubble tea!` //no comma needed if last
+      };
+
+      break;
+
+    case 2:
+      today = "Tuesday";
+      break;
+
+    case 3:
+      today = "Wednesday";
+      break;
+
+    default:
+      today = "Not sure what day it is!";
+
+
+
+  }
+
+  document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+
+  //alert(today);
+  console.log(coffee); 
