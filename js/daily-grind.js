e76ba5c832f5c08e610559a39751bfb4ce67b293
pgm369 <priissgm@gmail.com>
2023-06-06 13:51:27 -0700

    changes

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -19,32 +19,65 @@ function coffeeTemplate(coffee) {
     		 <img src="${coffee.pic}" alt="${coffee.alt}"    id="coffee" />
             <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
             `;
-
+}
 
 let myDate = new Date();
 
 let today = myDate.getDay();
 //today = 3;	
 
-switch(today){ 
-     case 2:
-         today = "Tuesday";
-     break;
 
-     case 3:
-         today = "Wednesday";
-     break;
+if(urlParams.has("day")){//from querystring
+  today = urlParams.get("day");
+}
+
+today = parseInt(today);
+
+//console.log(today);
+
+switch(today){ 
+
+    case 1:
+        today = "Monday";
+        coffee = {
+          name: "Bubble Tea",
+          color: "pink",
+          pic: "images/bubble-tea.jpg",
+          alt: "A picture of a Bubble Tea",
+          day: "Monday",
+          desc: `I like me some bubble tea!` //no comma needed if last
+        };
+
+    break;
+
+    case 2:
+        today = "Tuesday";
+        coffee = {
+          name: "Drip",
+          color: "black",
+          pic: "images/bubble-tea.jpg",
+          alt: "A picture of a drip coffee",
+          day: "Tuesday",
+          desc: `Give it to me Straight` //no comma needed if last
+        };
+
+    break;
+
+    case 3:
+        today = "Wednesday";
+    break;
 
     default:
-      today = "Not sure what day it is!";
+        today = "Not sure what day it is!";
 
 
 
   }
 
   document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
-
+  document.querySelector("html").style.backgroundColor = myColor;
 
   //alert(today);
   console.log(coffee); 
-}
+
+
