53d73ef85e2172c4315f7579815d97174ff460a2
pgm369 <priissgm@gmail.com>
2023-06-07 14:07:01 -0700

    updates index

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -21,7 +21,18 @@ function coffeeTemplate(coffee) {
             `;
 }
 
+
+
+const queryString = window.location.search;
+      
+console.log(queryString);
+    
+const urlParams = new URLSearchParams(queryString);
+
+
 let myDate = new Date();
+
+let coffee = "";
 
 let today = myDate.getDay();
 //today = 3;	
@@ -37,11 +48,24 @@ today = parseInt(today);
 
 switch(today){ 
 
+  case 0:
+    today = "Sunday";
+    coffee = {
+      name: "Caramel Latte",
+      color: "#e4bc84",
+      pic: "images/caramel-latte.jpg",
+      alt: "A picture of a Caramel latte",
+      day: "Sunday",
+      desc: `Espresso, frothed milk, and caramel sauce!` //no comma needed if last
+    };
+
+break;
+
     case 1:
         today = "Monday";
         coffee = {
           name: "Bubble Tea",
-          color: "pink",
+          color: "#ecdcc2",
           pic: "images/bubble-tea.jpg",
           alt: "A picture of a Bubble Tea",
           day: "Monday",
@@ -54,18 +78,67 @@ switch(today){
         today = "Tuesday";
         coffee = {
           name: "Drip",
-          color: "black",
-          pic: "images/bubble-tea.jpg",
+          color: "#37251b",
+          pic: "images/drip.jpg",
           alt: "A picture of a drip coffee",
           day: "Tuesday",
-          desc: `Give it to me Straight` //no comma needed if last
+          desc: `Give it to me Straight!` //no comma needed if last
         };
 
     break;
 
     case 3:
         today = "Wednesday";
+        coffee = {
+          name: "Mocha",
+          color: "#ccae88",
+          pic: "images/mocha.jpg",
+          alt: "A picture of a mocha",
+          day: "Wednesday",
+          desc: `If you love chocolate and coffee, Mocha is made for you!` //no comma needed if last
+        };
+
     break;
+
+    case 4:
+        today = "Thursday";
+        coffee = {
+          name: "Cold Brew",
+          color: "#80411e",
+          pic: "images/cold-brew.jpg",
+          alt: "A picture of a Cold brew",
+          day: "Thursday",
+          desc: `Cold pressed coffee! Sweet and Smooth!` //no comma needed if last
+        };
+
+    break;
+
+    case 5:
+        today = "Friday";
+        coffee = {
+          name: "Frappaccino",
+          color: "#6F4E37",
+          pic: "images/frappaccino.jpg",
+          alt: "A picture of a Frappaccino",
+          day: "Friday",
+          desc: `Blended iced coffee topped with whipped cream!` //no comma needed if last
+        };
+
+    break;
+
+    case 6:
+        today = "Saturday";
+        coffee = {
+          name: "Pumpkin Spice Latte",
+          color: "#ffcd91",
+          pic: "images/pumpkin-spice-latte.jpg",
+          alt: "A picture of a pumpkin-spice-latte",
+          day: "Saturday",
+          desc: `A mix of pumpkin flavors, steamed milk, and espresso topped with whip cream!` //no comma needed if last
+        };
+
+    break;
+
 
     default:
         today = "Not sure what day it is!";
@@ -75,7 +148,7 @@ switch(today){
   }
 
   document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
-  document.querySelector("html").style.backgroundColor = myColor;
+  document.querySelector("html").style.backgroundColor = coffee.color;
 
   //alert(today);
   console.log(coffee); 
